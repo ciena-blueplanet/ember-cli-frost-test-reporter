d4dee04b0a5a87999a72886a470ae9fbc2e02303
Seena <seenarowhani95@gmail.com>
2016-05-27 14:54:57 -0400

    toggle visibility on tester, and transitions and what not

--- vendor/ember-cli-mocha/test-loader.js
+++ vendor/ember-cli-mocha/test-loader.js
@@ -4,7 +4,10 @@ jQuery(document).ready(function() {
   // Declare `expect` as a global here instead of as a var in individual tests.
   // This avoids jshint warnings re: `Redefinition of 'expect'`.
   window.expect = chai.expect;
-
+  $('head').append('<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">');
+  $('.toggle').click(function(){
+    $('#ember-testing-container').toggleClass('hidden');
+  })
   var TestLoader = require('ember-cli/test-loader')['default'];
   TestLoader.prototype.shouldLoadModule = function(moduleName) {
     return moduleName.match(/[-_]test$/) || moduleName.match(/\.jshint$/);
